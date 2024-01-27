e66acf9c8ae0bd42a90037807f8872f60c0effa7
ronaldngounou <landryzinkoue@yahoo.fr>
2024-01-27 16:22:35 -0500

    data processed and cleaned

--- ingest.py
+++ ingest.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import hashlib
 import pandas as pd
 from bs4 import BeautifulSoup
 from langchain.document_loaders import RecursiveUrlLoader
@@ -102,13 +104,62 @@ def remove_trailing_characters(input_file_path, output_file_path):
 
     # Using regular expression to replace unwanted characters
     # This will remove occurrences of multiple \n and \t
-    processed_content = re.sub(r'[\t\n]+', ' ', content)
+    content = content.replace('\\n', ' ').replace('\\', '')
+
+        # Replace multiple spaces with a single space using regex
+    content = re.sub(r'\s+', ' ', content)
 
     with open(output_file_path, 'w', encoding='utf-8') as file:
-        file.write(processed_content)
+        file.write(content)
+
+def generate_hash(content):
+    return hashlib.sha256(str(content).encode()).hexdigest()
+
+def has_website_changed(new_data, filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            old_data = file.read()
+            old_hash = generate_hash(old_data)
+            new_hash = generate_hash(new_data)
+            return old_hash != new_hash
+    except FileNotFoundError:
+        # File not found, so it's the first run
+        return True
 
 # Call the function
 load_college_admission_docs()
 
 # Replace 'input.txt' and 'output.txt' with your file paths
-remove_trailing_characters('scraped_data.txt', 'processed_data.txt')
+
+def read_old_content(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    return None
+
+def write_content_to_file(content, file_path):
+    with open(file_path, 'w', encoding = 'utf-8') as file:
+        file.write(str(content))
+
+def main():
+    #website_url = 'http://example.com'
+    file_path = 'scraped_data.txt'
+
+    # Assuming load_college_admission_docs() scrapes and returns website content
+    current_content = load_college_admission_docs()  # Ensure this function is defined
+    old_content = read_old_content(file_path)
+
+    # If old content exists, compare hashes
+    if old_content and generate_hash(current_content) != generate_hash(old_content):
+        print("Website has changed. Updating content.")
+        write_content_to_file(current_content, file_path)
+    elif not old_content:
+        print("Initial run: Saving current content.")
+        write_content_to_file(current_content, file_path)
+    else:
+        print("No changes detected in the website.")
+    
+    remove_trailing_characters('scraped_data.txt', 'processed_data.txt')
+
+if __name__ == "__main__":
+    main()
